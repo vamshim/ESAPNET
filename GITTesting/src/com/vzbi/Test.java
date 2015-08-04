1fc0f275a310efb198c757b47e65eb57ed283917
v545586 <v545586@tusfl000vm114c8.vdsi.ent.verizon.com>
2015-08-04 13:48:25 +0530

    checkin 2222

--- GITTesting/src/com/vzbi/Test.java
+++ GITTesting/src/com/vzbi/Test.java
@@ -8,6 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println(" INSIDE MAIN METHOD ");
 		System.out.println(" 111111111111111 ");
+		System.out.println(" 22222222222222 ");
 	}
 
 }
